5e68a8cf611ff2ecacc8c6591acd5f00a496382f
mishlahatulhusna <36910792+mishlahatulhusna@users.noreply.github.com>
2019-01-02 09:47:58 +0700

    Add files via upload

---
+++ Praktikum11/AutoNim.java
@@ -0,0 +1,21 @@
+package Praktikum11;
+
+
+public class AutoNim {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // NIM : Tahun Masuk + Kode Prodi + Nomor Urut Registrasi
+        String ta = "2015";
+        String kd_prodi = "153";
+        String no_reg = "0001";
+        String nim;
+        
+        nim = ta.substring(2, 4)+kd_prodi+no_reg;
+        
+        System.out.println("NIM : "+nim);
+    }
+    
+}
---
+++ Praktikum11/Demo2.java
@@ -0,0 +1,16 @@
+package Praktikum11;
+
+
+public class Demo2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String teks="Java";
+        System.out.println("teks = Java");
+        System.out.println("equals(JAVA) = "+teks.equals("JAVA"));
+        System.out.println("equalsIgnoreCase(JAVA) = "+teks.equalsIgnoreCase("JAVA"));
+    }
+    
+}
